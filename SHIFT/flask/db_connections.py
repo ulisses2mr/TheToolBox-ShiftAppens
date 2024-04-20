2df0100df488fb665e7e2f17442149e3f445b65d
Mateus Flamínio <mflaminio482@gmail.com>
2024-04-20 23:51:43 +0100

    db tratada em principio

--- SHIFT/flask/db_connections.py
+++ SHIFT/flask/db_connections.py
@@ -32,6 +32,8 @@ def landing_page():
     DB Toolbox
     <br/>
     """
+    
+
 @app.route("/create_user", methods=['POST'])
 def create_user():
     payload = flask.request.get_json()
@@ -69,6 +71,84 @@ def create_user():
 
     return flask.jsonify(response)
 
+
+@app.route("/user_info/<id>", methods=['GET'])
+def get_user(id):
+    conn = db_connection()
+    cur = conn.cursor()
+    
+    try:
+        statement = """select name, username, location
+                    from utilizador
+                    where id = %s"""
+        
+        cur.execute(statement, (id,))
+        
+        rows = cur.fetchall()
+        
+        row = rows[0]
+        results = {
+            "name": row[0],
+            "username": row[1],
+            "location": row[2]
+        }
+                
+        response = {'status': StatusCodes['success'], 'results': results}
+        
+            
+    except (Exception, sdb.DatabaseError) as error:
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+        
+    finally:
+        if conn is not None:
+            conn.close()
+    
+    return flask.jsonify(response)
+
+
+@app.route("/new_tool", methods=['POST'])
+def create_tool():
+    payload = flask.request.get_json()
+    conn = db_connection()
+    cur = conn.cursor()
+
+    if 'id' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'id value not in payload'}
+        return flask.jsonify(response)
+    
+    id = payload['id']
+    ferramenta_type = payload['type']
+    brand = payload['brand']
+    image = payload['image']
+    price = payload['price']
+    
+    with open(image, "rb") as img_file:
+       imageData = img_file.read()
+       
+    statement = "INSERT INTO ferramenta VALUES(%s, %s, %s, %s, %s)"
+    values = (id, ferramenta_type, imageData, brand, price)
+       
+    try:
+        cur.execute(statement, values)
+
+        # commit the transaction
+        conn.commit()
+        response = {'status': StatusCodes['success'], 'results': f'Inserted tool'}
+
+    except (Exception, sdb.DatabaseError) as error:
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+        # an error occurred, rollback
+        conn.rollback()
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return flask.jsonify(response)
+    
+    
+    
 # Coloca uma proposta
 @app.route("/new_proposal", methods=['POST'])
 def create_proposal():
@@ -83,23 +163,18 @@ def create_proposal():
 
     id = payload['id']
     date_start = payload['date_start']
-    ferramenta_id = payload['ferramenta_id'] 
-    ferramenta_type = payload['ferramenta_type']
-    ferramenta_brand = payload['ferramenta_brand']
-    image = payload['image']
-    price = payload['price'], 
+    ferramenta_id = payload['ferramenta_id']
     utilizador_id = payload['utilizador_id']
 
-    with open(image, "rb") as img_file:
-       imageData = img_file.read()
     # parameterized queries, good for security and performance
-    statement = 'INSERT INTO oferta VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    statement = 'INSERT INTO oferta VALUES (%s, %s, %s, %s, %s)'
     #values = (1, '2024-04-20', True, 1, "Martelo do José", "Martelo", imageData, "balls", 1.00, 1)
     #values = (2, '2024-04-20', False, 2, "Martelo do Chico", "Martelo", image, "balls", 1.50, 2)
-    values = (id, date_start, 1, ferramenta_id, ferramenta_type, imageData, ferramenta_brand, price, utilizador_id)
+    values = (id, date_start, 1, ferramenta_id, utilizador_id)
 
     try:
         cur.execute(statement, values)
+        cur.execute("INSERT INTO emprestimo_ferramenta_toolbox VALUES (%s, %s)", (id, 1))
 
         # commit the transaction
         conn.commit()
@@ -131,20 +206,13 @@ def create_request():
     id = payload['id']
     date_start = payload['date_start']
     ferramenta_id = payload['ferramenta_id'] 
-    ferramenta_type = payload['ferramenta_type']
-    ferramenta_brand = payload['ferramenta_brand']
-    image = payload['image']
-    price = payload['price'], 
     utilizador_id = payload['utilizador_id']
     
-    with open(image, "rb") as img_file:
-       imageData = img_file.read()
-    
     # parameterized queries, good for security and performance
-    statement = 'INSERT INTO pedido VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    statement = 'INSERT INTO pedido VALUES (%s, %s, %s, %s, %s)'
     #values = (1, '2024-04-20', True, 1, "Martelo do José", "Martelo", imageData, "balls", 1.00, 1)
     #values = (2, '2024-04-20', False, 2, "Martelo do Chico", "Martelo", image, "balls", 1.50, 2)
-    values = (id, date_start, 1, ferramenta_id, ferramenta_type, imageData, ferramenta_brand, price, utilizador_id)
+    values = (id, date_start, 1, ferramenta_id, utilizador_id)
 
     try:
         cur.execute(statement, values)
@@ -172,9 +240,9 @@ def get_all_proposals():
     cur = conn.cursor()
     
     try:
-        cur.execute("""select id, ferramenta_id, ferramenta_type, ferramenta_brand, ferramenta_price, utilizador_id, date_start, ferramenta_photo
-                    from oferta
-                    where isProposal = 1;""")
+        cur.execute("""select oferta.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo
+                    from oferta, ferramenta
+                    where isProposal = 1 and ferramenta_id = ferramenta.id;""")
         
         rows = cur.fetchall()
         
@@ -215,9 +283,9 @@ def get_all_request():
     cur = conn.cursor()
     
     try:
-        cur.execute("""select id, ferramenta_id, ferramenta_type, ferramenta_brand, ferramenta_price, utilizador_id, date_start, ferramenta_photo
-                    from pedido
-                    where isProposal = 1;""")
+        cur.execute("""select pedido.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo
+                    from pedido, ferramenta
+                    where isProposal = 1 and ferramenta_id = ferramenta.id;""")
         
         rows = cur.fetchall()
         
@@ -259,9 +327,9 @@ def get_user_proposals(id):
     cur = conn.cursor()
     
     try:
-        statement = """select id, ferramenta_id, ferramenta_type, ferramenta_brand, ferramenta_price, date_start, ferramenta_photo
-                    from oferta
-                    where isProposal = 1 and utilizador_id = %s;"""
+        statement = """select oferta.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo
+                    from oferta, ferramenta
+                    where isProposal = 1 and ferramenta_id = ferramenta.id and utilizador_id = %s;"""
         
         cur.execute(statement, (id,))
         
@@ -301,9 +369,9 @@ def get_user_requests(id):
     cur = conn.cursor()
     
     try:
-        statement = """select id, ferramenta_id, ferramenta_type, ferramenta_brand, ferramenta_price, date_start, ferramenta_photo
-                    from pedido
-                    where isProposal = 1 and utilizador_id = %s;"""
+        statement = """select pedido.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo
+                    from pedido, ferramenta
+                    where isProposal = 1 and ferramenta_id = ferramenta.id and utilizador_id = %s;"""
         
         cur.execute(statement, (id,))
         
@@ -336,8 +404,10 @@ def get_user_requests(id):
             conn.close()
     
     return flask.jsonify(response)
- 
-@app.route("/accepted_proposals/")
+
+# Não sei se isto funciona ou se é necessário
+'''
+@app.route("/accepted_proposals/<id>")
 def get_accepted_proposals(id):
     conn = db_connection()
     cur = conn.cursor()
@@ -373,12 +443,92 @@ def get_accepted_proposals(id):
             conn.close()
             
     return flask.jsonify(response)
+'''
+
+@app.route("/proposals/type/<search>", methods=['GET'])
+def search_by_type(search):
+    conn = db_connection()
+    cur = conn.cursor()
+    
+    try:
+        statement = f"""select oferta.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo
+                    from oferta, ferramenta
+                    where isProposal = 1 and ferramenta_id = ferramenta.id and ferramenta_type like '{search}%';"""
+        
+        cur.execute(statement, tuple())
+
+        rows = cur.fetchall()
+        results = []
+        
+        for row in rows:
+            content = {
+                "id": int(row[0]), 
+                "ferramenta_id": int(row[1]), 
+                "ferramenta_type": row[2],
+                "ferramenta_brand": row[3],
+                "ferramenta_price": float(row[4]),
+                "utilizador_id": int(row[5]),
+                "date_start": row[6],
+                "ferramenta_photo": base64.b64encode(row[7]).decode('utf-8')
+                #"ferramenta_photo": row[8]    
+            }
+        
+        results.append(content)
+        
+        response = {'status': StatusCodes['success'], 'results': results}
+        
+    except (Exception, sdb.DatabaseError) as error:
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+        
+    finally:
+        if conn is not None:
+            conn.close()
+    
+    return flask.jsonify(response)
 
 
 
+@app.route("/proposals/location/<search>")
+def search_by_location(search):
+    conn = db_connection()
+    cur = conn.cursor()
+    
+    try:
+        statement = f"""select oferta.id, ferramenta_id, ferramenta_type, brand, price, utilizador_id, date_start, photo, location
+                        from oferta, ferramenta, emprestimo_ferramenta_toolbox as t, toolbox
+                        where oferta.id = t.oferta_id and toolbox.id = t.toolbox_id and isProposal = 1 and location like '{search}%';"""
+        
+        cur.execute(statement, tuple())
 
-
-
+        rows = cur.fetchall()
+        results = []
+        
+        for row in rows:
+            content = {
+                "id": int(row[0]), 
+                "ferramenta_id": int(row[1]), 
+                "ferramenta_type": row[2],
+                "ferramenta_brand": row[3],
+                "ferramenta_price": float(row[4]),
+                "utilizador_id": int(row[5]),
+                "date_start": row[6],
+                "ferramenta_photo": base64.b64encode(row[7]).decode('utf-8'),
+                "location": row[8]
+                #"ferramenta_photo": row[8]    
+            }
+        
+            results.append(content)
+        
+        response = {'status': StatusCodes['success'], 'results': results}
+        
+    except (Exception, sdb.DatabaseError) as error:
+        response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+        
+    finally:
+        if conn is not None:
+            conn.close()
+    
+    return flask.jsonify(response)
 
 
 
