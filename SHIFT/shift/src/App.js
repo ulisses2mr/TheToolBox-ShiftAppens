4112602547706fd58b8b42820658efeb963f662b
Ulisses Rodrigues <137835003+ulisses2mr@users.noreply.github.com>
2024-04-20 14:45:35 +0100

    Funciona

--- SHIFT/shift/src/App.js
+++ SHIFT/shift/src/App.js
@@ -1,43 +1,15 @@
-
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Login/Login';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
-const App = () => {
-  <BrowserRouter>
+export default function App() {
+  return (
+    <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Login/>} />
       </Routes>
     </BrowserRouter>
+  );
 };
 
-export default App;
-
-/*
-
-<Routes>
-      <Route path="/login" element={<Login />} ></Route> 
-      <Route path="/main"></Route> 
-    </Routes> 
-import React, {Fragment} from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import Login from "./Login/Login.jsx"
-
-function App() {
-
-return (
-    <BrowserRouter>
-      <Fragment>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-      </Fragment>
-    </BrowserRouter>
-  );
-}
-
-export default App;*/
