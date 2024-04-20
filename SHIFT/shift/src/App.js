9434f7e310b9c8e471194c44c8eaf816d5e26118
Pedro Anjinho <pedroanjinhosilva04@gmail.com>
2024-04-21 00:20:03 +0100

    WOW Frontend pagina!!!!!!!!!!!!
    Yay

--- SHIFT/shift/src/App.js
+++ SHIFT/shift/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Offers from './Offers/Offers';
-import Tool from './Tool_Page/Tool_Page';
+import Loan from './Loan/Loan';
 import { BrowserRouter} from 'react-router-dom';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/tool" element={<Tool/>} />
+        <Route path="/loan" element={<Loan/>} />
         <Route path="/offers" element={<Offers/>} />
       </Routes>
     </BrowserRouter>
