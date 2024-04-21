c998eee06d320b78223f8a625e4766be66c6f7f3
Ulisses Rodrigues <137835003+ulisses2mr@users.noreply.github.com>
2024-04-21 12:14:10 +0100

    NAVBAR

--- SHIFT/shift/src/Components/Navbar/Navbar.jsx
+++ SHIFT/shift/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,8 @@ import Add from '../../Assets/menuadd.svg'
 import Home from '../../Assets/menuhome.svg'
 import Profile from '../../Assets/menuprofile.svg'
 import Logo from '../../Assets/menulogo.svg'
-
+import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function hideMenu() {
     // Get all elements with class "menu_element"
@@ -23,13 +24,25 @@ function hideMenu() {
     });
 }
 
+function toOffers(){
+    window.location.href = '/offers';
+}
+
+function toLoan(){
+    window.location.href = '/loan';
+}
+
+function toProfile(){
+    window.location.href = '/profile';
+}
+
 const Navbar = () => (
     <div className="navbar_component">
    <div class="navbar_section1">
    <img id="menu_icon" src={Menu} onClick={hideMenu}></img>
-   <img class="menu_element" src={Home}></img>
-   <img class="menu_element" src={Profile}></img>
-   <img class="menu_element" src={Add}></img>
+   <img class="menu_element" src={Home} onClick={toOffers}></img>
+   <img class="menu_element" src={Profile} onClick={toProfile}></img>
+   <img class="menu_element" src={Add} onClick={toLoan}></img>
    </div>
    <img id="menu_logo" src={Logo}></img>
     </div>
